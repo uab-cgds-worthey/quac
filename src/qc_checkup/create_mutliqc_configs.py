b317c78f0c4fada095d1c72c9163d8cd8146407f
Manavalan Gajapathy <manag@uab.edu>
2021-04-06 11:29:22 -0500

    creates multiqc config from template - #12

--- src/qc_checkup/create_mutliqc_configs.py
+++ src/qc_checkup/create_mutliqc_configs.py
@@ -1,8 +1,12 @@
+"""
+Creates multiqc config file based on jinja templating and values from qc-checkup config file.
+"""
+
 from jinja2 import Template
 import yaml
 
 
-def main(template_f, qc_config):
+def main(template_f, qc_config, outfile):
 
     with open(qc_config) as file_handle:
         config = yaml.safe_load(file_handle)
@@ -11,6 +15,7 @@ def main(template_f, qc_config):
         template = Template(file_.read())
 
     data = template.render(
+        # fastq screen
         fastq_screen_human_min=config["fastq_screen"]["Human percentage"]["min"],
         fastq_screen_human_max=config["fastq_screen"]["Human percentage"]["max"],
         fastq_screen_mouse_min=config["fastq_screen"]["Mouse percentage"]["min"],
@@ -25,15 +30,38 @@ def main(template_f, qc_config):
         fastq_screen_nohits_max=config["fastq_screen"]["No hits percentage"]["max"],
         fastq_screen_others_min=config["fastq_screen"]["others percentage"]["min"],
         fastq_screen_others_max=config["fastq_screen"]["others percentage"]["max"],
+        # qualimap
         qualimap_avg_gc_min=config["qualimap"]["avg_gc"]["min"],
         qualimap_avg_gc_max=config["qualimap"]["avg_gc"]["max"],
         qualimap_mean_coverage_min=config["qualimap"]["mean_coverage"]["min"],
         qualimap_median_coverage_min=config["qualimap"]["median_coverage"]["min"],
+        qualimap_mean_median_coverage_ratio_min=config["qualimap"]["mean_cov:median_cov"]["min"],
+        qualimap_mean_median_coverage_ratio_max=config["qualimap"]["mean_cov:median_cov"]["max"],
+        qualimap_median_insert_size_min=config["qualimap"]["median_insert_size"]["min"],
         qualimap_percentage_aligned_min=config["qualimap"]["percentage_aligned"]["min"],
         qualimap_percentage_aligned_max=config["qualimap"]["percentage_aligned"]["max"],
+        # verifybamid
+        verifybamid_freemix_min=config["verifybamid"]["FREEMIX"]["min"],
+        verifybamid_freemix_max=config["verifybamid"]["FREEMIX"]["max"],
+        # bcftools_stats
+        bcftools_stats_number_of_records_min=config["bcftools_stats"]["number_of_records"]["min"],
+        bcftools_stats_number_of_records_max=config["bcftools_stats"]["number_of_records"]["max"],
+        bcftools_stats_number_of_SNPs_min=config["bcftools_stats"]["number_of_SNPs"]["min"],
+        bcftools_stats_number_of_SNPs_max=config["bcftools_stats"]["number_of_SNPs"]["max"],
+        bcftools_stats_number_of_indels_min=config["bcftools_stats"]["number_of_indels"]["min"],
+        bcftools_stats_number_of_indels_max=config["bcftools_stats"]["number_of_indels"]["max"],
+        bcftools_stats_perc_snps_min=config["bcftools_stats"]["perc_snps"]["min"],
+        bcftools_stats_perc_snps_max=config["bcftools_stats"]["perc_snps"]["max"],
+        bcftools_stats_perc_indels_min=config["bcftools_stats"]["perc_indels"]["min"],
+        bcftools_stats_perc_indels_max=config["bcftools_stats"]["perc_indels"]["max"],
+        bcftools_stats_tstv_min=config["bcftools_stats"]["tstv"]["min"],
+        bcftools_stats_tstv_max=config["bcftools_stats"]["tstv"]["max"],
+        bcftools_stats_heterozygosity_ratio_min=config["bcftools_stats"]["heterozygosity_ratio"][
+            "min"
+        ],
     )
 
-    with open("test.yaml", "w") as fh:
+    with open(outfile, "w") as fh:
         fh.write(data)
 
     return None
@@ -42,4 +70,5 @@ def main(template_f, qc_config):
 if __name__ == "__main__":
     TEMPLATE_F = "configs/multiqc_config_template.jinja2"
     QC_CONFIG = "configs/qc_checkup/qc_checkup_config.yaml"
-    main(TEMPLATE_F, QC_CONFIG)
+    OUTFILE = "configs/multiqc_config.yaml"
+    main(TEMPLATE_F, QC_CONFIG, OUTFILE)
