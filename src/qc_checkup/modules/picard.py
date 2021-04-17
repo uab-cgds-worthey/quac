809742c70d840762db2c173ffbaf05e9e59dcaf6
Manavalan Gajapathy <manag@uab.edu>
2021-04-16 22:20:03 -0500

    refactors picard checkup to single outfile setup

--- src/qc_checkup/modules/picard.py
+++ src/qc_checkup/modules/picard.py
@@ -5,22 +5,24 @@ from common import present_within_range, qc_logger, write_to_yaml_file
 LOGGER = qc_logger(__name__)
 
 
-def check_metrics(report_file, picard_asm_config, outfile):
+def check_thresholds(results_dict, pass_fail, report_file, config):
 
-    LOGGER.info(f"Reading multiqc's Picard-AlignmentSummaryMetrics report file: {report_file}")
+    LOGGER.info(f"Reading multiqc's Picard-module report file: {report_file}")
 
     df = pd.read_csv(report_file, sep="\t", index_col="Sample")
     # perc_columns = [x for x in df.columns if "percentage" in x]
 
-    results_dict = {}
-    pass_fail = set()
+    # results_dict = {}
+    # pass_fail = set()
     for sample_name, row in df.iterrows():
         LOGGER.info(f"Working on sample: {sample_name}")
 
-        results_dict[sample_name] = {}
-        for qc_metric in picard_asm_config.keys():
+        if sample_name not in results_dict:
+            results_dict[sample_name] = {}
+
+        for qc_metric in config.keys():
             # PCT in the name is misleading. They are actually fractions!
-            if qc_metric == "% Q30_BASES":
+            if qc_metric == "perc_Q30_BASES":
                 total_reads = row["TOTAL_BASES"]
                 q30_bases = row["Q30_BASES"]
                 value = (q30_bases / total_reads) * 100
@@ -28,13 +30,38 @@ def check_metrics(report_file, picard_asm_config, outfile):
                 value = row[qc_metric] * 100
             else:
                 value = row[qc_metric]
-            minimum = picard_asm_config[qc_metric]["min"]
-            maximum = picard_asm_config[qc_metric]["max"]
+            minimum = config[qc_metric]["min"]
+            maximum = config[qc_metric]["max"]
 
             result = present_within_range(value, minimum, maximum)
             results_dict[sample_name][f"{qc_metric}_val"] = float(value)
             results_dict[sample_name][qc_metric] = "pass" if result else "fail"
             pass_fail.add(result)
+
+    # write results to file
+    # write_to_yaml_file(results_dict, outfile)
+
+    # summarize results to one term
+
+    return results_dict, pass_fail
+
+
+def picard(config_dict, asm_infile, qym_infile, outfile):
+
+    results_dict = {}
+    pass_fail = set()
+
+    # picard - AlignmentSummaryMetrics
+    LOGGER.info("-" * 40)
+    results_dict, pass_fail = check_thresholds(
+        results_dict, pass_fail, asm_infile, config_dict["AlignmentSummaryMetrics"]
+    )
+
+    # picard - QualityYieldMetrics
+    LOGGER.info("-" * 40)
+    results_dict, pass_fail = check_thresholds(
+        results_dict, pass_fail, qym_infile, config_dict["QualityYieldMetrics"]
+    )
 
     # write results to file
     write_to_yaml_file(results_dict, outfile)
