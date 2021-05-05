6553b3a63556c180eb1d62aa28f6bb97c57bef69
Manavalan Gajapathy <manag@uab.edu>
2021-05-05 17:08:31 -0500

    refactors a bit

--- src/qc_checkup/modules/variant_freq_by_contig.py
+++ src/qc_checkup/modules/variant_freq_by_contig.py
@@ -9,7 +9,6 @@ def read_file(f):
 
     df = pd.read_csv(f, sep="\t", names=["contig", "length", "variant_count"], index_col="contig")
     df["var_freq"] = ((df["variant_count"] / df["length"]) * 100).round(4)
-    print(df.head())
 
     return df
 
@@ -25,27 +24,26 @@ def stat_by_chromosome(bcftools_index_f, sample_name, variant_freq_config, outfi
 
     chrom_list = [f"chr{x}" for x in list(range(1, 23)) + ["X", "Y"]]
     results_dict = {sample_name: {}}
-    pass_fail = []
-
+    pass_or_fail = set()
     for chrom in chrom_list:
         minimum = variant_freq_config[chrom]["min"]
         maximum = variant_freq_config[chrom]["max"]
 
-        print(chrom)
         if chrom in df.index:
             variant_freq = df.loc[chrom, "var_freq"]
-            print(chrom, variant_freq, minimum, maximum)
             result = present_within_range(variant_freq, minimum, maximum)
-            results_dict[sample_name][chrom] = float(f"{variant_freq:.3f}")
+            results_dict[sample_name][f"{chrom}_val"] = float(f"{variant_freq:.3f}")
         else:
-            results_dict[sample_name][chrom] = 0
             result = False
-        pass_fail.append(result)
+            results_dict[sample_name][f"{chrom}_val"] = 0
+
+        results_dict[sample_name][chrom] = "pass" if result else "fail"
+        pass_or_fail.add(result)
 
     # write results to file
     write_to_yaml_file(results_dict, outfile)
 
     # summarize results to one term
-    qc_check_status = "pass" if all(pass_fail) else "fail"
+    qc_check_status = "pass" if all(pass_or_fail) else "fail"
 
     return qc_check_status
